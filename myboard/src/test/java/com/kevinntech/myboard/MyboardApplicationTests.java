8a054eaf1423a85e8b032ad51d75eb35d2bb46f0
kevin <kevinntech@nate.com>
2020-09-13 11:54:25 +0900

    init

---
+++ myboard/src/test/java/com/kevinntech/myboard/MyboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kevinntech.myboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
